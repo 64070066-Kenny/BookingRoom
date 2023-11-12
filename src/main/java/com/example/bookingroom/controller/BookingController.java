be6157c2ccc85d0f74b65be2aff0c2ce2e727ec8
64070066-Kenny <88500591+64070066-Kenny@users.noreply.github.com>
2023-11-13 01:27:46 +0700

    Add files via upload

--- src/main/java/com/example/bookingroom/controller/BookingController.java
+++ src/main/java/com/example/bookingroom/controller/BookingController.java
@@ -3,6 +3,7 @@ package com.example.bookingroom.controller;
 import com.example.bookingroom.pojo.DataBooking;
 import com.example.bookingroom.repository.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class BookingController {
     @Autowired
@@ -39,51 +42,49 @@ public class BookingController {
 
 
     @RequestMapping(value = "/BookingByName", method = RequestMethod.GET)
-    public ResponseEntity<List<Map<String, String>>> showData(@RequestBody MultiValueMap<String, String> b) {
-        Map<String, String> m = b.toSingleValueMap();
-        String name = m.get("name");
-
+    public ResponseEntity<List<Map<String, String>>> showData(@RequestParam String name) {
         // Assuming you have a data source with DataBooking objects
         List<DataBooking> bookingList = bookingService.findDataBooking();
 
-        List<Map<String, String>> entriesWithSameName = new ArrayList<>();
+        // Filter and sort the data based on bookingDate
+        List<Map<String, String>> entriesWithSameName = bookingList.stream()
+                .filter(booking -> name.equals(booking.getName()))
+                .sorted(Comparator.comparing(DataBooking::getBookingDate))
+                .map(booking -> {
+                    Map<String, String> entry = new HashMap<>();
+                    entry.put("_id", booking.get_id());
+                    entry.put("roomName", booking.getRoomName());
+                    entry.put("participants", String.valueOf(booking.getParticipants()));
+                    entry.put("purpose", booking.getPurpose());
+                    entry.put("name", booking.getName());
+                    entry.put("bookingDate", booking.getBookingDate());
+                    entry.put("startTime", booking.getStartTime());
+                    entry.put("endTime", booking.getEndTime());
+                    entry.put("status", booking.getStatus());
+                    entry.put("email", booking.getEmail());
+                    return entry;
+                })
+                .collect(Collectors.toList());
 
-        // Iterate through the data and find entries with the same name
-        for (DataBooking booking : bookingList) {
-            if (name.equals(booking.getName())) {
-                Map<String, String> entry = new HashMap<>();
-                entry.put("roomName", booking.getRoomName());
-                entry.put("participants", String.valueOf(booking.getParticipants()));
-                entry.put("purpose", booking.getPurpose());
-                entry.put("name", booking.getName());
-                entry.put("bookingDate", booking.getBookingDate());
-                entry.put("startTime", booking.getStartTime());
-                entry.put("endTime", booking.getEndTime());
-                entry.put("status", booking.getStatus());
-                entry.put("email", booking.getEmail());
-                entriesWithSameName.add(entry);
-            }
-        }
-
-        // Return the list of entries with the same name
+        // Return the sorted list of entries with the same name
         return ResponseEntity.ok(entriesWithSameName);
     }
 
 
-    @RequestMapping(value = "/addBooking", method = RequestMethod.POST)
-    public ResponseEntity<?> createData(@RequestBody MultiValueMap<String, String> b) {
-        Map<String, String> m = b.toSingleValueMap();
-        String roomName = m.get("roomName");
-        String bookingDate = m.get("bookingDate");
-        String startTime = m.get("startTime");
-        String endTime = m.get("endTime");
+
+    @RequestMapping(value = "/addBooking", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createData(@RequestBody DataBooking requestBody) {
+        String roomName = requestBody.getRoomName();
+        String bookingDate = requestBody.getBookingDate();
+        String startTime = requestBody.getStartTime();
+        String endTime = requestBody.getEndTime();
 
         // Check if the start time is before the end time
         LocalTime newLocalStartTime = LocalTime.parse(startTime, DateTimeFormatter.ofPattern("HH:mm"));
         LocalTime newLocalEndTime = LocalTime.parse(endTime, DateTimeFormatter.ofPattern("HH:mm"));
 
         if (!newLocalStartTime.isBefore(newLocalEndTime)) {
-            return ResponseEntity.badRequest().body("Start time should be before end time.");
+            return ResponseEntity.badRequest().body("{\"error\": \"Start time should be before end time.\"}");
         }
 
         // Fetch all existing bookings from the database
@@ -103,13 +104,11 @@ public class BookingController {
         }
 
         // If no overlap is found and start time is before end time, create the new booking
-        DataBooking dataBooking = bookingService.createDataBooking(new DataBooking(
-                null, m.get("roomName"), Integer.parseInt(m.get("participants")), m.get("purpose"), m.get("name"),
-                m.get("bookingDate"), m.get("startTime"), m.get("endTime"), "Waiting for Approval",
-                m.get("email")));
+        DataBooking dataBooking = bookingService.createDataBooking(requestBody);
 
         return ResponseEntity.ok(dataBooking);
     }
+
 
 
 
@@ -131,20 +130,19 @@ public class BookingController {
                 LocalTime newLocalStartTime = LocalTime.parse(startTime, DateTimeFormatter.ofPattern("HH:mm"));
                 LocalTime newLocalEndTime = LocalTime.parse(endTime, DateTimeFormatter.ofPattern("HH:mm"));
 
-                // Check for room, date, and time overlap with other bookings
-                for (DataBooking otherBooking : existingBookings) {
-                    if (!otherBooking.get_id().equals(id)) {
+                // Check if only start and end times are being updated
+                if (!existingBooking.getStartTime().equals(startTime) || !existingBooking.getEndTime().equals(endTime)) {
+                    // Check for room, date, and time overlap with other bookings
+                    for (DataBooking otherBooking : existingBookings) {
+                        if (!otherBooking.get_id().equals(id)) {
+                            LocalTime otherLocalStartTime = LocalTime.parse(otherBooking.getStartTime(), DateTimeFormatter.ofPattern("HH:mm"));
+                            LocalTime otherLocalEndTime = LocalTime.parse(otherBooking.getEndTime(), DateTimeFormatter.ofPattern("HH:mm"));
 
-                        LocalTime otherLocalStartTime = LocalTime.parse(otherBooking.getStartTime(), DateTimeFormatter.ofPattern("HH:mm"));
-                        LocalTime otherLocalEndTime = LocalTime.parse(otherBooking.getEndTime(), DateTimeFormatter.ofPattern("HH:mm"));
-
-                        if(!newLocalStartTime.isBefore(newLocalEndTime) ){
-                            return false;
-                        }
-                        // Check for time overlap with other bookings
-                        if (!(newLocalEndTime.isBefore(otherLocalStartTime) || newLocalStartTime.isAfter(otherLocalEndTime)) && otherBooking.getRoomName().equals(roomName) && otherBooking.getBookingDate().equals(bookingDate)) {
-                            // There is an overlap in time with another booking for the same room and date, return false
-                            return false;
+                            // Check for time overlap with other bookings
+                            if (!(newLocalEndTime.isBefore(otherLocalStartTime) || newLocalStartTime.isAfter(otherLocalEndTime)) && otherBooking.getRoomName().equals(roomName) && otherBooking.getBookingDate().equals(bookingDate)) {
+                                // There is an overlap in time with another booking for the same room and date, return false
+                                return false;
+                            }
                         }
                     }
                 }
@@ -160,12 +158,12 @@ public class BookingController {
         }
         return false;
     }
-    
 
-    @RequestMapping(value = "/deleteBooking", method = RequestMethod.POST)
-    public boolean deleteData(@RequestBody MultiValueMap<String, String> b){
-        Map<String, String> m = b.toSingleValueMap();
-        String id = m.get("_id");
+
+
+    @RequestMapping(value = "/deleteBooking", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public boolean deleteData(@RequestBody Map<String, String> requestBody) {
+        String id = requestBody.get("_id");
 
         List<DataBooking> existingBookings = bookingService.findDataBooking();
         for (DataBooking existingBooking : existingBookings) {
@@ -178,5 +176,8 @@ public class BookingController {
     }
 
 
+
 }
+
+
 
